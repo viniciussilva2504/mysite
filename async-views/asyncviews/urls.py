418269e5bdeac1b40ea952e41698c49a84873872
Vinic <vinic@gamestore.com>
2026-01-23 10:01:17 +0000

    Organiza requirements.txt com comentários explicativos

---
+++ async-views/asyncviews/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', views.async_view),
+    path('sync/', views.sync_view),
+]
