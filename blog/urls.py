66ed77938c84605922b74d5302eb554e803e39f0
Vinic <vinic@gamestore.com>
2026-02-01 19:20:02 +0000

    Implementa funcionalidades dos templates: views renderizam templates HTML corretamente

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from views.post_view import PostView
+from views.post_view import PostView, PostDetailView
 
 urlpatterns = [
-    path('', PostView.as_view(), name='home')
+    path('', PostView.as_view(), name='home'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
